34363b96dc6c164afc1ba76494f561bb7baf190a
KHS <huisu0721@naver.com>
2023-06-30 17:38:18 +0900

    0630 EndCommit

---
+++ src/components/storeaccount/BranchLocationInfo.js
@@ -0,0 +1,39 @@
+export default 
+[
+    {
+        content: '<div>오리역</div>', 
+        latlng: new kakao.maps.LatLng(37.339, 127.1094)
+    },
+    {
+        content: '<div>맥도날드</div>', 
+        latlng: new kakao.maps.LatLng(37.3408, 127.1088)
+    },
+    {
+        content: '<div>뉴식뷔페</div>', 
+        latlng: new kakao.maps.LatLng(37.3406, 127.108)
+    },
+    {
+        content: '<div>닭꼬치</div>',
+        latlng: new kakao.maps.LatLng(37.3399, 127.1095)
+    },
+    {
+        content: '<div>깨돌이김밥</div>',
+        latlng: new kakao.maps.LatLng(37.3392, 127.1093)
+    },
+    {
+        content: '<div>노랑통닭</div>',
+        latlng: new kakao.maps.LatLng(37.3404, 127.1102)
+    },
+    {
+        content: '<div>도미노피자</div>',
+        latlng: new kakao.maps.LatLng(37.3406, 127.1093)
+    },
+    {
+        content: '<div>구움몽</div>',
+        latlng: new kakao.maps.LatLng(37.3391, 127.1099)
+    },
+    {
+        content: '<div>판타스틱</div>',
+        latlng: new kakao.maps.LatLng(37.3399, 127.1099)
+    }
+];
