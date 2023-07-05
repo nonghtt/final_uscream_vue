4fd5a48019729df8220e10eb775a9f5582824318
haeburney <4ckdnjssu@naver.com>
2023-07-05 17:44:28 +0900

    스케줄 삭제 2023-07-05

--- src/components/schedule/ScheDule.vue
+++ src/components/schedule/ScheDule.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="sdBody">
+    <div id="menu">
+      <div>
+        <ScheduleModal />
+      </div>
+      <div>
+        <ScheduleAddModal />
+      </div>
+      <div>
+        <button @click="look30and10" class="btn btncolor">{{ lookMinute }}분 단위로 보기</button>
+      </div>
+    </div>
 
-    <!-- Button trigger modal -->
-    <button type="button" class="btn custom-btn" data-bs-toggle="modal" data-bs-target="#addSchedule" @click="add">
-      스케줄 등록(안씀 곧 지울예정)
-    </button>
-
-    <ScheduleModal />
-    <ScheduleAddModal />
-   
+    <!-- 캘린더 -->
+    <div id="fullCalendar">
+      <FullCalendar :options="calendarOptions" />
+    </div>
 
     <!-- 등록 Modal -->
     <div class="modal fade" id="addSchedule" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -38,33 +45,27 @@
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">취소</button>
-            <button type="button" class="btn custom-btn" data-bs-dismiss="modal">등록</button>
+            <button type="button" class="btn btncolor" data-bs-dismiss="modal">등록</button>
           </div>
         </div>
       </div>
     </div>
 
-    <!-- 캘린더 -->
-    <div id="fullCalendar">
-      <FullCalendar :options="calendarOptions" />
-    </div>
-  </div>
-
-
-  <!-- Modal -->
-  <div class="modal fade" id="scModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">{{empname}}</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <div>{{sdate}}</div>
-          <div>{{ starttime }} - {{ endtime }} </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btncolor" data-bs-dismiss="modal">삭제</button>
+    <!-- 삭제 Modal -->
+    <div class="modal fade" id="scModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">{{ empname }}</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div>{{ sdate }}</div>
+            <div>{{ starttime }} - {{ endtime }} </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btncolor" data-bs-dismiss="modal" @click="delsc">삭제</button>
+          </div>
         </div>
       </div>
     </div>
@@ -77,11 +78,12 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 //   , { Draggable }
 import timeGridPlugin from '@fullcalendar/timegrid';
+import momentPlugin from '@fullcalendar/moment';
 
 import ScheduleModal from '@/components/schedule/ScheduleModal.vue'
 import ScheduleAddModal from '@/components/schedule/ScheduleAddModal.vue'
 
-import {Modal} from 'bootstrap';
+import { Modal } from 'bootstrap';
 
 export default {
   name: 'ScheDule',
@@ -96,18 +98,19 @@ export default {
       //scheduleModal:false,
       //test: false,
       // 일정 클릭했을 때의 정보
-      snum : 0,
-      empname : '',
-      sdate : '',
-      starttime : '',
-      endtime : '', 
+      snum: 0,
+      empname: '',
+      sdate: '',
+      starttime: '',
+      endtime: '',
+      lookMinute: 10,
       calendarOptions: {
-        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin, momentPlugin],
         initialView: 'timeGridWeek',
         headerToolbar: {
           left: 'prev,next today',
           center: 'title',
-          right: 'dayGridMonth,timeGridWeek'
+          right: 'dayGridMonth,timeGridWeek',
         },
         droppable: true,
         editable: true,                 // 편집 가능하게 
@@ -117,13 +120,20 @@ export default {
         eventResize: this.workDayResize,
         timeZone: 'Asia/Seoul',
         height: 700,
-        views: {
+        views: {  // views 안에 제목?을 지정해줄 수 있음
           timeGridWeek: {
+            slotDuration: '00:30:00',
             scrollTime: '08:00:00',
-            allDaySlot: false
+            allDaySlot: false,
+            titleFormat: 'M[월] D[일]',
+            titleRangeSeparator: ' - ',
+          },
+          dayGridMonth: {
+            titleFormat: 'YYYY[년] M[월]'
           },
         },
         selectable: true,               // 달력 일자 드래그 설정가능
+
       },
     };
   },
@@ -166,79 +176,44 @@ export default {
 
   },
   methods: {
-    add() {
-      console.log('스케줄 추가');
-      const self = this;
-      const storeid = self.storeid;
-      self.$axios.get(`http://localhost:8085/emp/storeid/${storeid}`)
-        .then(function (res) {
-          if (res.status == 200 & res.data.flag == true) {
-            self.empList = res.data.list
-          } else {
-            console.log("에러 :" + res.status);
-          }
-        })
-      self.selectedEmp = 'none';
-    },
+    // 등록된 스케줄을 눌렀을 때
     workDayClick(info) {
       const self = this;
-      console.log("클릭 정보")
       console.log(info.event)
       const modal = document.getElementById('scModal');
-      console.log(modal)
-      //modal.setAttribute("aria-hidden", "false");
-     
-      console.log(info.event._def.defId)
-      console.log(info.event._def.title)
-      console.log(info.event._instance.range.start)
-      console.log(info.event._instance.range.end)
-      
-      let day = info.event._instance.range.start
-      
-      let startTime = JSON.stringify(info.event._instance.range.start).slice(12, 17)
-      startTime = startTime.replace('^0','').replace(':g','시 ').replace('$','분');
-      
-     
-      //startTime[startTime.length] = '분';
-      
-      let endTime = JSON.stringify(info.event._instance.range.end).slice(12, 17)
-      endTime = endTime.replace(':','시 ');
-      endTime += '분'
-      //endTime[startTime.length] = '분';
-      console.log(typeof(startTime))
 
-
-
+      let day = info.event._instance.range.start;
       const date = new Date(day);
       const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+      let startTime = JSON.stringify(info.event._instance.range.start).slice(12, 17)
+      startTime = startTime.replace(/^0/, '').replace(/:/g, '시 ').replace(/$/, '분');
+      let endTime = JSON.stringify(info.event._instance.range.end).slice(12, 17)
+      endTime = endTime.replace(/^0/, '').replace(/:/g, '시 ').replace(/$/, '분');
+
       self.sdate = `${date.toLocaleDateString('ko-KR', options)}`;
       self.starttime = startTime
       self.endtime = endTime
-
-      self.snum = info.event._def.defId
+      self.snum = info.event._def.publicId
       self.empname = info.event._def.title
       //self.sdate = info.event._instance.range.start
-     // self.starttime = info.event._instance.range.start
-     // self.endtime = info.event._instance.range.end
+      // self.starttime = info.event._instance.range.start
+      // self.endtime = info.event._instance.range.end
+      //console.log(self.calendarOptions.events[0])
 
       let myModal = new Modal(modal)
       myModal.show();
 
-  
+
     },
     workDayEdit(info) {
-      console.log("num :" + info.event._def.publicId);
-
       this.formatDate(info.event._instance.range.start, info.event._instance.range.end, info.event._def.publicId)
-
-
-
     },
     workDayResize(info) {
-      console.log(info.event);
+      // 시간 계산 함수 (드래그 수정 함수 )
       this.formatDate(info.event._instance.range.start, info.event._instance.range.end, info.event._def.publicId)
     },
-    // 시간 계산 함수 
+    // 시간 계산 함수  : 2023-07-05T08:10:00 이런 식으로 
     formatDate(start, end, num) {
       let startDateObj = new Date(start)
       startDateObj.setHours(startDateObj.getHours() - 9);
@@ -253,47 +228,39 @@ export default {
       endDateObj.setHours(endDateObj.getHours() - 9);
       let etime = String(endDateObj.getHours()).padStart(2, "0") + ":" + String(endDateObj.getMinutes()).padStart(2, "0")
       let endTime = `${year}-${month}-${day}T${etime}:00`;
-      console.log(endTime)
+      //console.log("formatDate : " + editDate + " / " + startTime + " / " + endTime + " / " + num)
 
-      console.log("formatDate : " + editDate + " / " + startTime + " / " + endTime + " / " + num)
+      // 드래그 수정 함수 
       this.editSchedule(editDate, startTime, endTime, num);
-
     },
-    // 수정 하기 
+    // 드래그 수정 함수 
     editSchedule(editDate, startTime, endTime, num) {
-      console.log(editDate)
-      console.log(startTime)
-      console.log(endTime)
-      console.log(num)
-
       let formData = new FormData();
       formData.append("snum", num);
       formData.append("sdate", editDate);
       formData.append("starttime", startTime);
       formData.append("endtime", endTime);
 
-
       const self = this;
       self.$axios.put('http://localhost:8085/schedule', formData)
         .then(function (res) {
-          console.log(res.status)
-          console.log(res.data.dto)
+          if (res.status == 200 && res.data.flag == true) {
+            console.log(res.data.dto)
+          } else {
+            console.log("에러 editSchedule() : " + res.status)
+          }
         })
 
     },
-    // 등록하기 전 직원 선택...............
+    // 등록하기 전 직원 선택
     empChange() {
       const self = this;
-
       let empnum = self.selectedEmp;
-      console.log(empnum)
+
       self.$axios.get(`http://localhost:8085/basicschedule/emp/${empnum}`)
         .then(function (res) {
-          console.log(res.status)
-          console.log(res.data)
           if (res.status == 200 && res.data.flag == true) {
-            console.log("성공")
-            console.log(res.data)
+            //console.log(res.data)
             self.empSelectedList = res.data.list;
           } else {
             console.log("에러 empChange() : " + res.status)
@@ -301,25 +268,89 @@ export default {
         })
 
     },
-    // testadd() {
-    //   this.test = true;
-    //   console.log(this.test)
-    // }
+    // 30분 단위로 보기 ↔ 10분 단위로 보기 
+    look30and10() {
+      // 현재 보여주고 있는 시간 불러오기 
+      let time = this.calendarOptions.views.timeGridWeek.slotDuration;
+
+      if (time === '00:30:00') {  // 10분 단위로 보고싶다.
+        this.calendarOptions.views.timeGridWeek.slotDuration = '00:10:00';
+        this.lookMinute = 30;
+      } else {                  // 30분 단위로 보고싶다.
+        this.calendarOptions.views.timeGridWeek.slotDuration = '00:30:00';
+        this.lookMinute = 10;
+      }
+      //this.calendarOptions.views.timeGridWeek.scrollTime = '08:00:00';
+    },
+    // 스케줄 삭제하기
+    delsc(){
+      const self = this;
+      console.log(self.snum)
+      let num = self.snum;
+      self.$axios.delete(`http://localhost:8085/schedule/${num}`)
+      .then(function(res){
+        if(res.status == 200 & res.data.flag == true){
+          console.log("삭제 성공");
+        } else {
+          console.log("삭제 에러 :" + res.status)
+        }
+      })
+      self.calendarOptions.events=[];
+
+      self.$axios.get('http://localhost:8085/schedule/storeid/' + self.storeid)
+      .then(function (res) {
+        if (res.status == 200 && res.data.flag == true) {
+          //console.log(res.data.list);
+          self.list = res.data.list;
+          console.log("길이 문제 : " + self.list.length)
+          //console.log(self.list[0].emp.empname);
+          self.list.forEach(item => {
+            //console.log(item.starttime);
+            const event = {
+              id: item.snum,
+              //groupId: item.emp.empnum,
+              title: item.emp.empname,
+              start: item.starttime,
+              end: item.endtime,
+              backgroundColor: item.emp.color,
+              borderColor: item.emp.color
+            };
+            // console.log("event");
+            // console.log(event);
+            self.calendarOptions.events.push(event);
+
+          });
+        }
+        // console.log(self.calendarOptions.events);
+      });
+    }
   }
 };
 </script>
   
 <style scoped>
 #sdBody {
+  display: flex;
   padding-top: 30px;
   padding-bottom: 30px;
-  width: 60%;
+  width: 80%;
   margin: 0 auto;
 }
 
 #fullCalendar {
-  margin-top: 50px;
+  /* float : left; */
+  flex: 1;
+  width:90%;
+  margin:0 auto;
+  padding-top:50px;
 }
+
+#menu{
+  margin-top: 50px;
+  flex: 0.2;
+  margin-right : 5px;
+}
+
 
 #scheduleAdd {
   float: right;
@@ -334,22 +365,17 @@ export default {
   text-decoration: none;
 }
 
-.custom-btn {
-  background-color: #B3C755;
-}
-
-.custom-btn:hover {
-  background-color: #99ab49;
-}
-
-.btncolor:hover{
+.btncolor:hover {
   background-color: #FFC67B;
-  color:#595959
+  color: #595959;
 }
-.btncolor{
-    color:#595959;
-    background-color: #bee96d;
-    font-weight: bolder ;
+
+.btncolor {
+  color: #595959;
+  background-color: #bee96d;
+  font-weight: bolder;
+  margin-top:5px;
+  margin-bottom:5px;
 }
 </style>
   
--- src/components/schedule/ScheduleAddModal.vue
+++ src/components/schedule/ScheduleAddModal.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            스케줄 직접 추가 (아직 준비중...)
+        <button type="button" class="btn btncolor" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            스케줄 추가
         </button>
 
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog  modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
@@ -18,7 +18,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btncolor">Save changes</button>
                     </div>
                 </div>
             </div>
@@ -30,4 +30,17 @@
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.btncolor:hover {
+  background-color: #FFC67B;
+  color: #595959;
+}
+
+.btncolor {
+  color: #595959;
+  background-color: #bee96d;
+  font-weight: bolder;
+  margin-top:5px;
+  margin-bottom:5px;
+}
+</style>
