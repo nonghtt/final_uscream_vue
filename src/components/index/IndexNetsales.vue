6791b939d13e17eaa4de11936feebaec3325b20b
YK <wyk160@naver.com>
2023-07-11 20:22:41 +0900

    IndexNetsales, storeLogin... 2023-07-11 20:22

--- src/components/index/IndexNetsales.vue
+++ src/components/index/IndexNetsales.vue
@@ -1,21 +1,17 @@
 <template>
     <div id="container">
-        <h2>매출</h2>
+        <h3 style="z-index=1">매출</h3>
 
-        <div v-if="accounttype == 1" class="content"> 
+        <div v-if="accounttype == 1" class="content" style="z-index=2">
             <div id="HQchart"></div>
-
-
-            
         </div>
 
-        <div v-if="accounttype == 2" class="content"> 
-            <div id="StoreChart"></div>
-            
+        <div v-if="accounttype == 2" class="content" style="z-index=2">
+            <div id="storeChart"></div>
         </div>
 
-         <!-- 라우터 링크 넣어주세요~! -->
-        <button class="btn" id="btncolor">더보기</button>
+        <!-- 라우터 링크 넣어주세요~! -->
+        <button class="btn" id="btncolor" style="z-index=3">더보기</button>
     </div>
 </template>
 
@@ -32,6 +28,7 @@ export default {
             id: sessionStorage.getItem("loginId"),
             accounttype: sessionStorage.getItem("accounttype"),
             list: [],
+            list2: []
         }
     },
     created: function () {
@@ -39,20 +36,18 @@ export default {
         echarts.use([GridComponent, BarChart, CanvasRenderer]);
         this.type();
     },
-    mounted () {
-        const currentDate = new Date();
-        this.year = currentDate.getFullYear();
-        this.month = currentDate.getMonth();
+    mounted() {
+
     },
     methods: {
-      type () {
-        if (this.accounttype === "1") {
-            this.getHQchart();
-        } else if (this.accounttype === "2") {
-            this.getHQchart();
-        }
-      },
-      getHQchart() {
+        type() {
+            if (this.accounttype === "1") {
+                this.getHQchart();
+            } else if (this.accounttype === "2") {
+                this.getStoreChart();
+            }
+        },
+        getHQchart() {
         const self = this;
 
         self.$axios.get(`http://localhost:8085/selling/dailysales`)
@@ -63,27 +58,23 @@ export default {
 
             const day = new Date(); // 현재 날짜 추출
 
-            for (let i = 4; i >= 0; i--) {  // 오늘부터 5일전 날짜까지 추출
-                const sellingdate = new Date(day.getTime() - i * 24 * 60 * 60 * 1000);
-                self[sellingdate + '-' + i + 'day'] = sellingdate;
+            for (let i = 3; i >= 1; i--) {  // 오늘부터 3일전 날짜까지 추출
+                const sellingdate = new Date(day.getTime() - i * 24 * 60 * 60 * 1000).toISOString().slice(0, 10);
+                self['salesDay-' + i] = sellingdate;
 
-                console.log(sellingdate);
-                
+                console.log(self['salesDay-' + i]);
+
                 const item = self.list.find(obj => {
-                    const itemDate = new Date(obj.SELLINGDATE);
-                    return itemDate.toDateString() === sellingdate.toDateString();
-                })
-   
+                const itemDate = obj.SELLINGDATE.substring(0, 10);
+                return itemDate.toString() === sellingdate.toString();
+                
+                });
 
                 if (item) {
-                    const totalprice = item.TOTALPRICE;
-                    self[sellingdate + totalprice] = totalprice;
-                    self.sellingdate = totalprice;
-                    console.log(self[sellingdate + '-' + i + 'day']);
+                const totalprice = item.TOTALPRICE;
+                self['salesDay-' + i + 'sales'] = totalprice;
                 }
             }
-
-        
 
             const myChart = echarts.init(document.getElementById('HQchart'));
 
@@ -91,31 +82,103 @@ export default {
                 xAxis: {
                 type: 'category',
                 data: [
-                    new Date(day.getTime() - 4 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10),
-                    new Date(day.getTime() - 3 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10),
-                    new Date(day.getTime() - 2 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10),
-                    new Date(day.getTime() - 1 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10),
-                    new Date(day).toISOString().slice(0, 10)
+                    self['salesDay-1'],
+                    self['salesDay-2'],
+                    self['salesDay-3']
                 ]
                 },
                 yAxis: {
-                type: 'value'
+                type: 'value',
+                interval: 2500000
                 },
                 series: [
                 {
                     data: [
-                    self[new Date(day.getTime() - 4 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10) + 'sales'],
-                    self[new Date(day.getTime() - 3 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10) + 'sales'],
-                    self[new Date(day.getTime() - 2 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10) + 'sales'],
-                    self[new Date(day.getTime() - 1 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10) + 'sales'],
-                    self[new Date(day).toISOString().slice(0, 10) + 'sales']
+                    self['salesDay-1sales'],
+                    self['salesDay-2sales'],
+                    self['salesDay-3sales']
+                    ],
+                    type: 'bar',
+                    barWidth: '20%',
+                    showBackground: true,
+                    backgroundStyle: {
+                    color: 'rgba(180, 180, 180, 0.2)'
+                    }
+                }
+                ]
+            };
+
+            myChart.setOption(option);
+            })
+            .catch(error => {
+            console.error(error);
+            });
+        },
+        getStoreChart() {
+        const storeId = this.id;
+
+        console.log(storeId);
+        const self = this;
+
+        self.$axios.get(`http://localhost:8085/selling/dailysales/${storeId}`)
+            .then(response => {
+            self.list2 = response.data.list;
+
+            console.log(self.list2);
+
+            const day = new Date(); // 현재 날짜 추출
+
+            for (let i = 3; i >= 1; i--) {  // 오늘부터 3일전 날짜까지 추출
+                const sellingdate = new Date(day.getTime() - i * 24 * 60 * 60 * 1000).toISOString().slice(0, 10);
+                self['salesDay-' + i] = sellingdate;
+
+                console.log(self['salesDay-' + i]);
+
+                const item = self.list2.find(obj => {
+                const itemDate = obj.SELLINGDATE.substring(0, 10);
+                return itemDate.toString() === sellingdate.toString();
+                
+                });
+
+                if (item) {
+                const totalprice = item.TOTALPRICE;
+                self['salesDay-' + i + 'sales'] = totalprice;
+                }
+            }
+
+            for (let i = 1; i <= 3; i++) {
+                console.log(self['salesDay-' + i + 'sales']);
+            }
+
+            const myChart = echarts.init(document.getElementById('storeChart'));
+
+            const option = {
+                xAxis: {
+                type: 'category',
+                data: [
+                    self['salesDay-1'],
+                    self['salesDay-2'],
+                    self['salesDay-3']
+                ]
+                },
+                yAxis: {
+                type: 'value',
+                interval: 500000
+                },
+                series: [
+                {
+                    data: [
+                    self['salesDay-1sales'],
+                    self['salesDay-2sales'],
+                    self['salesDay-3sales']
                     ],
                     tooltip: {
-                          valueFormatter: function (value) {
-                            return value.toLocaleString() + ' 원';
-                          }
-                        },
+                    valueFormatter: function (value) {
+                        return value.toLocaleString() + ' 원';
+                        }
+                    },
                     type: 'bar',
+                    barWidth: '20%',
                     showBackground: true,
                     backgroundStyle: {
                     color: 'rgba(180, 180, 180, 0.2)'
@@ -133,21 +196,26 @@ export default {
     }
 }
 
+
 </script>
 
 <style scoped>
 #container {
-    margin: 10px 10px;
+    /* margin: 10px 10px; */
     text-align: center;
     overflow: hidden;
-     /* 이 위는 바꾸지 마세요~ */
+    /* 이 위는 바꾸지 마세요~ */
+    display: flex; 
+    flex-direction: column; 
+    align-items: center; 
+    justify-content: center;
 }
 
-.content{
-    margin-top:5px;
+.content {
+    margin-top: 5px;
     overflow: auto;
-    max-height: 11em;
-     /* 이 위는 바꾸지 마세요~ */
+    /* max-height: 11em; */
+    /* 이 위는 바꾸지 마세요~ */
 }
 
 #btncolor:hover {
@@ -156,7 +224,7 @@ export default {
 }
 
 #btncolor {
-    margin-top:5px;
+    margin-top: 5px;
     color: #595959;
     background-color: #bee96d;
     font-weight: bolder;
@@ -165,7 +233,16 @@ export default {
 }
 
 #HQchart {
-    width:400px;
-    height:300px;
+    width: 500px;
+    height: 250px;
+    margin-top: -50px;
+    margin-left: 50px;
+}
+
+#storeChart {
+    width: 500px;
+    height: 250px;
+    margin-top: -50px;
+    margin-left: 50px;
 }
 </style>
--- src/components/storeaccount/storeLogin.vue
+++ src/components/storeaccount/storeLogin.vue
@@ -1,268 +1,495 @@
+
 <template>
-    <div id="login" class="login">
-      <!-- <div class="text_login">LOGIN</div> -->
-
-      <router-link class="logorouter" to="/location">로그인창 디자인 테스트</router-link>
-
-
-
-      
-      <div id="login_window">
-      <div class="brandname">USCREAM</div>
-        <div style="height:40px;"></div>
-          <div class="title_login" role="alert" style="font-size:25px; font-weight:bold;">
-        LOGIN</div>
-
-      
-         
-          
-          <div class="div_storeid">
-          <input class="input_storeid" type="text" v-model="storeid" required>
-          <label class="label_storeid">아이디</label>
-          <span class="span_storeid"></span>
+  <div :class="logincontainerClasses " class="logincontainer" id="logincontainer">
+    <!-- FORM SECTION -->
+    <div class="row">
+      <!-- SIGN UP -->
+      <div class="col align-items-center flex-col sign-up">
+        <div class="form-wrapper align-items-center">
+          <div class="form sign-up">
+            <div class="input-group">
+              <i class='bx bxs-user'></i>
+              <input type="text" placeholder="Username">
+            </div>
+            <div class="input-group">
+              <i class='bx bx-mail-send'></i>
+              <input type="email" placeholder="Email">
+            </div>
+            <div class="input-group">
+              <i class='bx bxs-lock-alt'></i>
+              <input type="password" placeholder="Password">
+            </div>
+            <div class="input-group">
+              <i class='bx bxs-lock-alt'></i>
+              <input type="password" placeholder="Confirm password">
+            </div>
+            <button>
+              Sign up
+            </button>
+            <p>
+              <span>
+                Already have an account?
+              </span>
+              <b onclick="toggle()" class="pointer">
+                Sign in here
+              </b>
+            </p>
           </div>
-  
-          
-          <div class="div_pwd">
-          <input class="input_pwd" type="password" v-model="pwd" required>
-          <label class="label_pwd">비밀번호</label>
-          <span class="span_pwd"></span>
         </div>
 
-        <br/>
-        <br/>
-        <br/>
-          <button class="btn_login" v-on:click="login">로그인하기</button>
-          </div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'storeLogin',
-    data(){
-      return{
-        storeid:'',
-        pwd:'',
-        accounttype:''
-      }
-    },
-    components: {
+      </div>
+      <!-- END SIGN UP -->
+      <!-- SIGN IN -->
+      <div class="col align-items-center flex-col sign-in">
+        <div class="form-wrapper align-items-center"  style="margin-bottom:5%; margin-left:-10%">
+          <div class="form sign-in">
+            <div class="input-group">
+              <i class='bx bxs-user'></i>
+              <input type="text" placeholder="Login ID" v-model="storeid" required>
+            </div>
+            <div class="input-group">
+              <i class='bx bxs-lock-alt'></i>
+              <input type="password" placeholder="Password" v-model="pwd" required>
+            </div>
+            <button v-on:click="login">
+              Log In
+            </button>
 
-    },
-    methods:{
-      login(){
-              const self = this;
-              const form = new FormData();
-              form.append('storeid', self.storeid);
-              form.append('pwd', self.pwd);
-              self.$axios.post('http://localhost:8085/store/login', form) //비동기요청
-              .then(function(res){
-                  if(res.status == 200){
-                      if(res.data.flag){
-                          sessionStorage.setItem('loginId', self.storeid)
-                          sessionStorage.setItem('accounttype', res.data.type)
-                          location.href='/about'
-                  }else{
-                      alert('로그인 실패')
-                  }
-              }else{
-                  alert('에러코드:' + res.status)
-              }
-              });
-          },
-      logout(){
-            sessionStorage.remove('storeid')
-            sessionStorage.remove('accounttype')
-            location.href='/'
+          </div>
+        </div>
+        <div class="form-wrapper">
+
+        </div>
+      </div>
+      <!-- END SIGN IN -->
+    </div>
+    <!-- END FORM SECTION -->
+    <!-- CONTENT SECTION -->
+    <div class="row content-row">
+      <!-- SIGN IN CONTENT -->
+      <div class="col align-items-center flex-col" style="margin-bottom:5%; margin-left:8%">
+        <div class="text sign-in">
+          <h2>
+            Welcome <br> USCREAM
+          </h2>
+
+        </div>
+        <div class="img sign-in">
+
+        </div>
+      </div>
+      <!-- END SIGN IN CONTENT -->
+      <!-- SIGN UP CONTENT -->
+      <div class="col align-items-center flex-col">
+        <div class="img sign-up">
+
+        </div>
+        <div class="text sign-up">
+          <h2>
+            Join with us
+          </h2>
+
+        </div>
+      </div>
+      <!-- END SIGN UP CONTENT -->
+    </div>
+    <!-- END CONTENT SECTION -->
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'storeLogin',
+  data() {
+    return {
+      storeid: '',
+      pwd: '',
+      accounttype: '',
+      logincontainerClasses:''
+    };
+  },
+  created: function () {
+   setTimeout(() => {
+     this.logincontainerClasses ="sign-in"
+     }, 100);
+  },
+  methods: {
+    login() {
+      const self = this;
+      const form = new FormData();
+      form.append('storeid', self.storeid);
+      form.append('pwd', self.pwd);
+      self.$axios
+        .post('http://localhost:8085/store/login', form)
+        .then(function(res) {
+          if (res.status === 200) {
+            if (res.data.flag) {
+              sessionStorage.setItem('loginId', self.storeid);
+              sessionStorage.setItem('accounttype', res.data.type);
+              location.href = '/about';
+            } else {
+              alert('로그인 실패');
+            }
+          } else {
+            alert('에러코드: ' + res.status);
           }
+        });
+      }
     }
   }
-  </script>
-  <style scoped>
-  
-  
-  /* div위에 div고정 */
-  .text_login{
-  position: absolute; left: 50%; top: 28%; 
-  transform: translate(-50%, -50%); text-align: center;
-  border-radius: 15%;
-  font-size:30px;
-  border:0;
-  background-color: rgb(128, 129, 128);
-  border-radius: 10%;
-  color:white;
-  /* div겹치기위한 속성, 숫자가 클수록 위로 옴 */
-  z-index:6;
+
+</script>
+
+<style> 
+:root {
+  --primary-color: #bee96d;
+  --secondary-color: #bee96d;
+  --black: #000000;
+  --white: #ffffff;
+  --gray: #efefef;
+  --gray-2: #757575;
+
+  --facebook-color: #4267B2;
+  --google-color: #DB4437;
+  --twitter-color: #1DA1F2;
+  --insta-color: #E1306C;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap');
+
+* {
+  font-family: 'Poppins', sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  height: 100vh;
+  overflow: hidden;
+}
+
+.logincontainer {
+  position: relative;
+  min-height: 100vh;
+  overflow: hidden;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  height: 100vh;
+}
+
+.col {
+  width: 50%;
+}
+
+.align-items-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.form-wrapper {
+  width: 100%;
+  max-width: 28rem;
+}
+
+.form {
+  padding: 1rem;
+  background-color: var(--white);
+  border-radius: 1.5rem;
+  width: 100%;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  transform: scale(0);
+  transition: .5s ease-in-out;
+  transition-delay: 1s;
+}
+
+.input-group {
+  position: relative;
+  width: 100%;
+  margin: 1rem 0;
+}
+
+.input-group i {
+  position: absolute;
+  top: 50%;
+  left: 1rem;
+  transform: translateY(-50%);
+  font-size: 1.4rem;
+  color: var(--gray-2);
+}
+
+.input-group input {
+  width: 100%;
+  padding: 1rem 3rem;
+  font-size: 1rem;
+  background-color: var(--gray);
+  border-radius: .5rem;
+  border: 0.125rem solid var(--white);
+  outline: none;
+}
+
+.input-group input:focus {
+  border: 0.125rem solid var(--primary-color);
+}
+
+.form button {
+  cursor: pointer;
+  width: 100%;
+  padding: .6rem 0;
+  border-radius: .5rem;
+  border: none;
+  background-color: var(--primary-color);
+  color: var(--white);
+  font-size: 1.2rem;
+  outline: none;
+}
+
+.form p {
+  margin: 1rem 0;
+  font-size: .7rem;
+}
+
+.flex-col {
+  flex-direction: column;
+}
+
+.social-list {
+  margin: 2rem 0;
+  padding: 1rem;
+  border-radius: 1.5rem;
+  width: 100%;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  transform: scale(0);
+  transition: .5s ease-in-out;
+  transition-delay: 1.2s;
+}
+
+.social-list>div {
+  color: var(--white);
+  margin: 0 .5rem;
+  padding: .7rem;
+  cursor: pointer;
+  border-radius: .5rem;
+  cursor: pointer;
+  transform: scale(0);
+  transition: .5s ease-in-out;
+}
+
+.social-list>div:nth-child(1) {
+  transition-delay: 1.4s;
+}
+
+.social-list>div:nth-child(2) {
+  transition-delay: 1.6s;
+}
+
+.social-list>div:nth-child(3) {
+  transition-delay: 1.8s;
+}
+
+.social-list>div:nth-child(4) {
+  transition-delay: 2s;
+}
+
+.social-list>div>i {
+  font-size: 1.5rem;
+  transition: .4s ease-in-out;
+}
+
+.social-list>div:hover i {
+  transform: scale(1.5);
+}
+
+.facebook-bg {
+  background-color: var(--facebook-color);
+}
+
+.google-bg {
+  background-color: var(--google-color);
+}
+
+.twitter-bg {
+  background-color: var(--twitter-color);
+}
+
+.insta-bg {
+  background-color: var(--insta-color);
+}
+
+.pointer {
+  cursor: pointer;
+}
+
+.logincontainer.sign-in .form.sign-in,
+.logincontainer.sign-in .social-list.sign-in,
+.logincontainer.sign-in .social-list.sign-in>div,
+.logincontainer.sign-up .form.sign-up,
+.logincontainer.sign-up .social-list.sign-up,
+.logincontainer.sign-up .social-list.sign-up>div {
+  transform: scale(1);
+}
+
+.content-row {
+  position: absolute;
+  top: 0;
+  left: 0;
+  pointer-events: none;
+  z-index: 6;
+  width: 100%;
+}
+
+.text {
+  margin: 4rem;
+  color: var(--white);
+}
+
+.text h2 {
+  font-size: 3.5rem;
+  font-weight: 800;
+  margin: 2rem 0;
+  transition: 1s ease-in-out;
+}
+
+.text p {
+  font-weight: 600;
+  transition: 1s ease-in-out;
+  transition-delay: .2s;
+}
+
+.img img {
+  width: 30vw;
+  transition: 1s ease-in-out;
+  transition-delay: .4s;
+}
+
+.text.sign-in h2,
+.text.sign-in p,
+.img.sign-in img {
+  transform: translateX(-250%);
+}
+
+.text.sign-up h2,
+.text.sign-up p,
+.img.sign-up img {
+  transform: translateX(250%);
+}
+
+.logincontainer.sign-in .text.sign-in h2,
+.logincontainer.sign-in .text.sign-in p,
+.logincontainer.sign-in .img.sign-in img,
+.logincontainer.sign-up .text.sign-up h2,
+.logincontainer.sign-up .text.sign-up p,
+.logincontainer.sign-up .img.sign-up img {
+  transform: translateX(0);
+}
+
+/* BACKGROUND */
+
+.logincontainer::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100vh;
+  width: 300vw;
+  transform: translate(35%, 0);
+  background-image: linear-gradient(-45deg, var(--primary-color) 0%, var(--secondary-color) 100%);
+  transition: 1s ease-in-out;
+  z-index: 6;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  border-bottom-right-radius: max(50vw, 50vh);
+  border-top-left-radius: max(50vw, 50vh);
+}
+
+.logincontainer.sign-in::before {
+  transform: translate(0, 0);
+  right: 50%;
+}
+
+.logincontainer.sign-up::before {
+  transform: translate(100%, 0);
+  right: 50%;
+}
+
+/* RESPONSIVE */
+
+@media only screen and (max-width: 425px) {
+
+  .logincontainer::before,
+  .logincontainer.sign-in::before,
+  .logincontainer.sign-up::before {
+    height: 100vh;
+    border-bottom-right-radius: 0;
+    border-top-left-radius: 0;
+    z-index: 0;
+    transform: none;
+    right: 0;
   }
-  
-  /* 버튼 색 바꾸기 */
-  .btn_login{
-  background-color: #8eb443;
-  border:0;
-  color:white;
-  font-size:20px;
-  width:300px;
-  font-weight:bold;
-  border:none;
-  outline:none;
+
+  /* .logincontainer.sign-in .col.sign-up {
+        transform: translateY(100%);
+    } */
+
+  .logincontainer.sign-in .col.sign-in,
+  .logincontainer.sign-up .col.sign-up {
+    transform: translateY(0);
   }
-  
-  /* 로그인창 윈도우 위치조정 */
-  #login_window{
-  width: 400px;height: 450px;border: 1px solid #dcdcdc;
-  position: absolute; left: 50%; top: 50%; 
-  transform: translate(-50%, -50%); text-align: center;
-  border-radius: 15%;
-  box-shadow: 20px 20px 20px grey;
-  background-color: white;
+
+  /* @media (min-width: 768px)
+  .logincontainer, .logincontainer-md, .logincontainer-sm {
+    max-width: 100%;
+  } */
+
+  .content-row {
+    align-items: flex-start !important;
   }
-  
-  .brandname{
-   color: #8eb443;
-   font-size:30px;
-   font-weight: bold;
+
+  .content-row .col {
+    transform: translateY(0);
+    background-color: unset;
   }
-  .title_login{
-    background-color: #8eb443;
-    color:white;
-    margin:0;
-    /* border-radius:  0 0; */
+
+  .col {
+    width: 100%;
+    position: absolute;
+    padding: 2rem;
+    background-color: var(--white);
+    border-top-left-radius: 2rem;
+    border-top-right-radius: 2rem;
+    transform: translateY(100%);
+    transition: 1s ease-in-out;
   }
-  
-  
-        /* 마우스 올리면 버튼 올라오는 기능 */
-      /* .btn_login:hover,
-      .btn_login:focus-visible{
-          transform: translateY(-1rem);
-      } */
-  
-      
-     
-  
-      * {
+
+  .row {
+    align-items: flex-end;
+    justify-content: flex-end;
+  }
+
+  .form,
+  .social-list {
+    box-shadow: none;
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
   }
-  
-  
-  /* input(storeid) 창 꾸미기 */
-  
-  .div_storeid {
-    position: relative;
-    width: 300px;
-    margin-left: 50px;
-    margin-top: 50px;
+
+  .text {
+    margin: 0;
   }
-  
-  .input_storeid {
-    font-size: 15px;
-    color: #222222;
-    width: 300px;
-    border: none;
-    border-bottom: solid #aaaaaa 1px;
-    padding-bottom: 10px;
-    padding-left: 10px;
-    position: relative;
-    background: none;
-    z-index: 5;
+
+  .text p {
+    display: none;
   }
-  
-  /* .input_storeid::placeholder { color: #aaaaaa; } */
-  .input_storeid::placeholder { color: #aaaaaa; }
-  .input_storeid:focus { outline: none; }
-  
-  .span_storeid {
-    display: block;
-    position: absolute;
-    bottom: 0;
-    left: 0%;  /* right로만 바꿔주면 오 - 왼 */
-    background-color: #666;
-    width: 0;
-    height: 2px;
-    border-radius: 2px;
-    transition: 0.5s;
+
+  .text h2 {
+    margin: .5rem;
+    font-size: 2rem;
   }
-  
-  .label_storeid {
-    position: absolute;
-    color: #aaaaaa;
-    left: 10px;
-    font-size: 20px;
-    bottom: 8px;
-    transition: all .2s;
-  }
-  
-  .input_storeid:focus ~ .label_storeid, .input_storeid:valid ~ .label_storeid {
-    font-size: 16px;
-    bottom: 40px;
-    /* 커서올렸을때 글자가 변하는 색 >>>>> 나중에 상의해서 변경하는 */
-    /* color: #666; */
-    color:#666;
-    font-weight: bold;
-  }
-  
-  .input_storeid:focus ~ .span_storeid, .input_storeid:valid ~ .span_storeid {
-    width: 100%;
-   
-  }
-  
-       
-  /* input태그(pwd) 꾸미기 */
-  .div_pwd {
-    position: relative;
-    width: 300px;
-    margin-left: 50px;
-    margin-top: 50px;
-  }
-  
-  .input_pwd {
-    font-size: 15px;
-    color: #222222;
-    width: 300px;
-    border: none;
-    border-bottom: solid #aaaaaa 1px;
-    padding-bottom: 10px;
-    padding-left: 10px;
-    position: relative;
-    background: none;
-    z-index: 5;
-  }
-  
-  .input_pwd::placeholder { color: #aaaaaa; }
-  .input_pwd:focus { outline: none; }
-  
-  .span_pwd {
-    display: block;
-    position: absolute;
-    bottom: 0;
-    left: 0%;  /* right로만 바꿔주면 오 - 왼 */
-    background-color: #666;
-    width: 0;
-    height: 2px;
-    border-radius: 2px;
-    transition: 0.5s;
-  }
-  
-  .label_pwd {
-    position: absolute;
-    color: #aaa;
-    left: 10px;
-    font-size: 20px;
-    bottom: 8px;
-    transition: all .2s;
-  }
-  
-  .input_pwd:focus ~ .label_pwd, .input_pwd:valid ~ .label_pwd {
-    font-size: 16px;
-    bottom: 40px;
-    color: #666;
-    font-weight: bold;
-  }
-  
-  .input_pwd:focus ~ .span_pwd, .input_pwd:valid ~ .span_pwd {
-    width: 100%;
-  }
-    
-  </style>
+}
+</style>
